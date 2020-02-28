819f20b52e46d6b7b1c1ab119958cb8f110c64cc
rr1980 <rener1980@googlemail.com>
2020-02-28 22:46:58 +0100

    theme handling

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  urlGET:  'http://localhost:8080/isga-webstart/service/webclient/get',
+  urlPOST: 'http://localhost:8080/isga-webstart/service/webclient/post'
 };
